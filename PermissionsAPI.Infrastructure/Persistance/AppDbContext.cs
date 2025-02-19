ca49cb788cba291f7c750667254655da8232df38
g-esman <gastonesman@gmail.com>
2025-02-19 16:26:36 -0300

    agrego MediatR y patron CQRS con el command; Agrego dockerfile y DockerCompose

--- PermissionsAPI.Infrastructure/Persistance/AppDbContext.cs
+++ PermissionsAPI.Infrastructure/Persistance/AppDbContext.cs
@@ -5,6 +5,9 @@ namespace PermissionsAPI.Infrastructure.Persistence
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<PermissionType> PermissionType { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- PermissionsAPI.Infrastructure/obj/Debug/net9.0/PermissionsAPI.Infrastructure.AssemblyInfo.cs
+++ PermissionsAPI.Infrastructure/obj/Debug/net9.0/PermissionsAPI.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermissionsAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39bff20a54b2b764f6d7e82cc17588b7ac3b4c4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c8c523de002e75ba97e27e6accb95280c393d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermissionsAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermissionsAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
